38d0b8ee011b79fb82f77f64e14c96cb4683f4c7
Morgante Pell <morgante.pell@morgante.net>
2012-12-29 21:20:25 -0500

    testing

--- routes/oauth.js
+++ routes/oauth.js
@@ -36,9 +36,9 @@ server.serializeClient(function(client, done) {
 
 server.deserializeClient(function(id, done) {
 	// return done( false );
+	console.log( 'ID: ' + id );
 	Client.findOne({id: id}, function(err, client) {
 		if (err) { return done(err); }
-		console.log( client );
 		return done(null, client);
 	});
 });
