4bcae743cf3005f3f9deb9e450343467557783bd
Morgante Pell <morgante.pell@morgante.net>
2012-12-29 20:47:13 -0500

    remove sensitive info

--- models/clients.js
+++ models/clients.js
@@ -1,16 +1,3 @@
-// hard list of clients
-// var clients = [
-//     {
-// 		id: '1',
-// 		name: 'RDC Status',
-// 		clientId: 'W6zBUBqmHWh3MwnKVsiQx9ez',
-// 		clientSecret: 'geF6A6RaBYKVr4i',
-// 		scopes: [
-// 			'user.me.netID'
-// 		]
-// 	}
-// ];
-
 // we need mongoose
 var mongoose = require('mongoose');
 
@@ -23,28 +10,3 @@ var clientSchema = mongoose.Schema({
 });
 
 var Client = module.exports = mongoose.model('Client', clientSchema);
-
-// exports.find = function(id, done) {
-//   for (var i = 0, len = clients.length; i < len; i++) {
-//     var client = clients[i];
-//     if (client.id === id) {
-//       return done(null, client);
-//     }
-//   }
-//   return done(null, null);
-// };
-// 
-// exports.findByClientId = function(clientId, done) {
-//   for (var i = 0, len = clients.length; i < len; i++) {
-//     var client = clients[i];
-//     if (client.clientId === clientId) {
-//       return done(null, client);
-//     }
-//   }
-//   return done(null, null);
-// };
-// 
-// exports.get = function( id, done ) {
-// 	
-// 	console.log( id );
-// }
