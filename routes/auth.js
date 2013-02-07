a18c7366bf66f30e832a74a7520a3e3c62dbea30
Morgante Pell <morgante.pell@morgante.net>
2013-02-07 14:47:02 +0400

    cut out the extra redirects/pages

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,7 @@ exports.start = function(req, res){
 	}
 		
 	// here we redirect to the NYU login and onto the nyu login step here
-	var redirect = 'https://login.nyu.edu/sso/UI/Login?goto=' + process.env.base_url + '/auth/nyu';
-	redirect = process.env.base_url + '/auth/nyu';
+	var redirect = process.env.base_url + '/auth/google';
 	res.redirect( redirect );
 };
 
@@ -26,12 +25,16 @@ exports.finish = function(req, res){
 };
 
 exports.nyu = function(req, res){
-	// this intermediary step uses a frame to ensure the NYU token has been passed along to Google
-	// it then redirects, via Javascript, to the Google authentication step
-	res.render("auth_nyu", {
-		title: "Authentication...",
-		next: process.env.base_url + '/auth/google'
-	});
+	var redirect = process.env.base_url + '/auth/google';
+	res.redirect( redirect );
+	// we no longer need to redirect more
+	// 
+	// // this intermediary step uses a frame to ensure the NYU token has been passed along to Google
+	// // it then redirects, via Javascript, to the Google authentication step
+	// res.render("auth_nyu", {
+	// 	title: "Authentication...",
+	// 	next: process.env.base_url + '/auth/google'
+	// });
 };
 
 exports.fail = function(req,res){	
